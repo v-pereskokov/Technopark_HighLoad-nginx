8be3649b90688bee414a50bcb84b01312234dd69
vladpereskokov <vladpereskokov@mail.ru>
2017-09-04 19:55:32 +0300

    add start func

--- server/server.go
+++ server/server.go
@@ -1,7 +1,21 @@
 package server
 
+import (
+	"github.com/vladpereskokov/Technopark_HighLoad-nginx/configs"
+	"log"
+	"net"
+)
+
 type Server struct {
 	protocol string
 	host     string
 	port     string
+}
+
+func (server *Server) Start(config *configs.Config) {
+	_, err := net.Listen("tcp", ":"+config.GetPort())
+	if err != nil {
+		panic("Failed start server: " + err.Error())
+	}
+	log.Print("Server started at " + config.GetPort() + " port")
 }
