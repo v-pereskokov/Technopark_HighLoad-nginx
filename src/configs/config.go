feddfc5076b65ce0a54b022c97b31795c2dad3ce
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 16:05:48 +0300

    move methods server config to server method

--- src/configs/config.go
+++ src/configs/config.go
@@ -1,36 +1,10 @@
 package configs
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "fmt"
 
 type Config struct {
 	Server *Server
 	Dir    *Dir
-}
-
-func (config *Config) GetNetwork() string {
-	return config.Server.Network
-}
-
-func (config *Config) GetProtocol() string {
-	return config.Server.Protocol
-}
-
-func (config *Config) GetHost() string {
-	return config.Server.Host
-}
-
-func (config *Config) GetPort() string {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = strconv.Itoa(config.Server.Port)
-	}
-
-	return port
 }
 
 func (config *Config) GetDir() string {
--- src/configs/server.go
+++ src/configs/server.go
@@ -1,8 +1,35 @@
 package configs
 
+import (
+	"os"
+	"strconv"
+)
+
 type Server struct {
 	Network  string `json:"network"`
 	Protocol string `json:"protocol"`
 	Host     string `json:"host"`
 	Port     int    `json:"port"`
+}
+
+func (config *Config) GetNetwork() string {
+	return config.Server.Network
+}
+
+func (config *Config) GetProtocol() string {
+	return config.Server.Protocol
+}
+
+func (config *Config) GetHost() string {
+	return config.Server.Host
+}
+
+func (config *Config) GetPort() string {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = strconv.Itoa(config.Server.Port)
+	}
+
+	return port
 }
