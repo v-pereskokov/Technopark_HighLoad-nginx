e77f52532675b85915b8bdd365896198ff3f27cf
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 13:14:23 +0300

    add request methods

--- src/models/request.go
+++ src/models/request.go
@@ -6,3 +6,15 @@ type Request struct {
 	method *Method
 	url    *url.URL
 }
+
+func (request *Request) GetPath() string {
+	if request.url != nil {
+		return request.url.Path
+	}
+
+	return ""
+}
+
+func (request *Request) set_path(path string) {
+	request.url.Path = path
+}
