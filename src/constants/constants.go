1131172f2aecdb173b876a830e17fb156e2208a2
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 19:40:34 +0300

    change constants

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -1,20 +1,5 @@
 package constants
 
-var (
-	CONTENT_TYPES = map[string]string{
-		".css":  "text/css",
-		".gif":  "image/gif",
-		".html": "text/html",
-		".jpeg": "image/jpeg",
-		".jpg":  "image/jpeg",
-		".js":   "text/javascript",
-		".json": "application/json",
-		".txt":  "application/text",
-		".png":  "image/png",
-		".swf":  "application/x-shockwave-flash",
-	}
-)
-
 const (
 	STRING_SEPARATOR     string = "\r\n"
 	HEADERS_END          string = STRING_SEPARATOR + STRING_SEPARATOR
