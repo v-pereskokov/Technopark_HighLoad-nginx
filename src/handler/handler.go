3a08aa0324198f11dc2fc839d9d34e508b1f5c08
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 13:23:29 +0300

    del comments

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -12,14 +12,13 @@ func (handler *Handler) Start(channel chan net.Conn) {
 	for {
 		conn := <-channel
 		buf := make([]byte, 1024)
-		// Read the incoming connection into the buffer.
+
 		_, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
 		}
-		// Send a response back to person contacting us.
+
 		conn.Write([]byte("Message received."))
-		// Close the connection when you're done with it.
 		conn.Close()
 	}
 }
