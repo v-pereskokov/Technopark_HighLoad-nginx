ce4346d84a85936c9b69dc98b2b3b1651111a2a4
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 16:07:02 +0300

    move methods dir config to dir model

--- src/configs/config.go
+++ src/configs/config.go
@@ -1,16 +1,6 @@
 package configs
 
-import "fmt"
-
 type Config struct {
 	Server *Server
 	Dir    *Dir
-}
-
-func (config *Config) GetDir() string {
-	config.Dir.FindDir()
-
-	fmt.Println(config.Dir.Path)
-
-	return config.Dir.Path
 }
--- src/configs/dir.go
+++ src/configs/dir.go
@@ -1,13 +1,24 @@
 package configs
 
-import "github.com/vladpereskokov/Technopark_HighLoad-nginx/src/utils"
+import (
+	"fmt"
+	"github.com/vladpereskokov/Technopark_HighLoad-nginx/src/utils"
+)
 
 type Dir struct {
 	Path string `json:"path"`
 }
 
-func (dir *Dir) FindDir() {
+func (dir *Dir) findDir() {
 	if dir.Path == "pwd" {
 		dir.Path = utils.Pwd()
 	}
+}
+
+func (config *Config) GetDir() string {
+	config.Dir.findDir()
+
+	fmt.Println(config.Dir.Path)
+
+	return config.Dir.Path
 }
