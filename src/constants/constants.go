c8c9f1b36c67bc6d4e31f36c6ba441ea9fe15e77
vladpereskokov <vladpereskokov@mail.ru>
2017-09-05 13:48:38 +0300

    del fool

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -33,7 +33,6 @@ const (
 	STRING_SEPARATOR     string = "\r\n"
 	HEADERS_END          string = STRING_SEPARATOR + STRING_SEPARATOR
 	HTTP_VERSION         string = "HTTP/1.1"
-	SERVER               string = "IliaBulavintsevServer"
 	INDEX_FILE           string = "/index.html"
 	ERROR_BODY_MIME_TYPE string = "text/html"
 	DEFAULT_MIME_TYPE    string = "text/html"
