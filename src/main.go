6f34b220b474dfeb661230a17bbb7362b81d838e
vladpereskokov <vladpereskokov@mail.ru>
2017-09-08 13:37:18 +0300

    add cpu count

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/vladpereskokov/Technopark_HighLoad-nginx/src/server"
 	"github.com/vladpereskokov/Technopark_HighLoad-nginx/src/utils"
 	"log"
+	"runtime"
 )
 
 const SERVER_CONFIG = "configs/server.json"
@@ -17,6 +18,8 @@ func main() {
 	if err != nil {
 		log.Panicf("can not init server config: %v", err)
 	}
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	httpServer := server.CreateServer(*serverConfig.GetServer())
 	httpServer.Start(handler.CreateHandler(serverConfig.Dir.Path))
